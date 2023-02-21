5ea8338b159eeb5c6f1ebfa466e47c2c6f96715b
Richard Hrmo <rhrmo@redhat.com>
2023-02-21 10:03:17 +0100

    refactor + bug fix when there are more comments about adding issue to advisory

--- pkg/jira/handler.go
+++ pkg/jira/handler.go
@@ -29,27 +29,23 @@ func (h *Handler) CreateJiraToErrataMap(ctx context.Context, issues []Issue) map
 			fmt.Printf("Didn't find the errata for the %s\n", i.Key)
 			continue
 		}
-		if issues, ok := errataToJira[errataID]; ok {
-			errataToJira[errataID] = append(issues, i)
-		} else {
-			errataToJira[errataID] = []Issue{i}
-		}
+		errataToJira[errataID] = append(errataToJira[errataID], i)
 	}
 	return errataToJira
 }
 
 func (h *Handler) FindErrataID(ctx context.Context, jiraIssue *Issue) string {
+	errataID := ""
 	for _, c := range jiraIssue.Fields.Comments.Comment {
 		if c.Author.EmailAddress == CommentAuthor && strings.Contains(c.Body, "This issue has been added to advisory") {
 			r, err := regexp.Compile(`advisory/\d+`)
 			if err != nil {
 				fmt.Printf("Can't compile the regex pattern: %v\n", err)
-				return ""
+				continue
 			}
 			subStr := r.FindString(c.Body)
-			errataID := strings.Split(subStr, "/")[1]
-			return errataID
+			errataID = strings.Split(subStr, "/")[1]
 		}
 	}
-	return ""
+	return errataID
 }
